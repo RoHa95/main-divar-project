59ace5e778e35fcce141fc342336245d9c4aa68f
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-04-20 22:25:05 +0330

    Creating a new category form and styling it

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import CategoryForm from "src/components/tamplates/CategoryForm";
 
 function AdminPage() {
-  return (
-    <div>AdminPage</div>
-  )
+  return <CategoryForm />;
 }
 
-export default AdminPage
+export default AdminPage;
