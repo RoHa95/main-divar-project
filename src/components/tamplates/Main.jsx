92fc0cae57ae596f35131f6075874972a8d3ed0c
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-04-22 18:16:51 +0330

    add style file to main component

--- src/components/tamplates/Main.jsx
+++ src/components/tamplates/Main.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import { sp } from 'src/utils/numbers';
+import React from "react";
+import { sp } from "src/utils/numbers";
 
-function Main({posts}) {
+import styles from "./Main.module.css";
+
+function Main({ posts }) {
   console.log(posts);
   return (
-    <div>
-      {posts.data.posts.map(post=>(
-        <div key={post._id}>
-          <div>
+    <div className={styles.container}>
+      {posts.data.posts.map((post) => (
+        <div key={post._id} className={styles.card}>
+          <div className={styles.info}>
             <p>{post.options.title}</p>
             <div>
               <p>{sp(post.amount)} تومان</p>
               <span>{post.options.city}</span>
             </div>
           </div>
-          <img src={`${import.meta.env.VITE_BASE_URL}${post.images[0]}`} alt="" />
+          <img
+            src={`${import.meta.env.VITE_BASE_URL}${post.images[0]}`}
+            alt=""
+          />
         </div>
       ))}
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
