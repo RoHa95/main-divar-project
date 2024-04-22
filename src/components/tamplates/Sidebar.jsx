29621cdcae28c4f0916966eb6c28c9a1bb86265f
Roqhaye Halimi <roqiahalimi95@gmail.com>
2024-04-22 16:57:47 +0330

    add style file to sidebar component

--- src/components/tamplates/Sidebar.jsx
+++ src/components/tamplates/Sidebar.jsx
@@ -2,24 +2,24 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { getCategory } from "src/services/admin";
 
+import styles from "./Sidebar.module.css";
+
 function Sidebar() {
   const { data } = useQuery(["get-categoties"], getCategory);
   console.log(data);
-  return <div>
-    <h4>
-دسته بندی ها
-    </h4>
-    <ul>
-        {
-            data?.data.map(category =>(
-                <li key={category._id}>
-                    <img src={`${category.icon}.svg`} alt="" />
-                    <p>{category.name}</p>
-                </li>
-            ))
-        }
-    </ul>
-  </div>;
+  return (
+    <div className={styles.sidebar}>
+      <h4>دسته بندی ها</h4>
+      <ul>
+        {data?.data.map((category) => (
+          <li key={category._id}>
+            <img src={`${category.icon}.svg`} alt="" />
+            <p>{category.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Sidebar;
